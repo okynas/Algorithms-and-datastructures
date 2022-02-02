e251458d15ebe4ef0a93b6ea21a6b709e72ee943
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-02-02 10:54:39 +0100

    Finish part 1 (snu-algorithm) of oppgave7 modul 5

--- Moduler/Modul5_Rekursiv_programmering/Oppgave7/src/Main.java
+++ Moduler/Modul5_Rekursiv_programmering/Oppgave7/src/Main.java
@@ -1,22 +1,11 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static String snu(String s, int n) {
-        ArrayList<Character> streng = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            streng.add(s.charAt(i));
+    public static void snu(String s, int n) {
+        if (n > 0) {
+            System.out.print(s.charAt(n-1));
+            snu(s.substring(0, n-1), n-1);
         }
-
-        //System.out.println(streng.remove(0));
-
-        if (streng.remove(0) != streng.remove(n-1)) {
-            return "Error";
-        }
-
-        String input = streng.toString();
-
-        return snu(input, input.length());
     }
 
     public static void metode2() {
@@ -25,7 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
         String input = "grebiøH";
-
         snu(input, input.length());
     }
 
