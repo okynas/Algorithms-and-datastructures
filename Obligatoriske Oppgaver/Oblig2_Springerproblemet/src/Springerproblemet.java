76b18f06acf00b40d7df4e23082efe7e0fdd723f
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-02-03 11:49:21 +0100

    Working oblig 2 - springerproblemet. (todo: add toString method)

--- Obligatoriske Oppgaver/Oblig2_Springerproblemet/src/Springerproblemet.java
+++ Obligatoriske Oppgaver/Oblig2_Springerproblemet/src/Springerproblemet.java
@@ -35,6 +35,17 @@ public class Springerproblemet {
 
     /**
      *
+     * @param x nye posisjon for x
+     * @param y nye poisjon for y
+     * @param board sjakkbrettet hvor springen skal bevege seg
+     * @return true hvis x og y er innenfor grensa (n x n), og x og y ikke er brukt fra før.
+     */
+    private static boolean checkAvailability(int x, int y, int[][] board) {
+        return x >= 0 && x < n && y >= 0 && y < n && board[x][y] == LEDIG;
+    }
+
+    /**
+     *
      * @param i - kolonner
      * @param j - rader
      * @return true hvis den finner en vei slik at springeren kan nå alle rutene i sjakkbrettet på n x n.
@@ -64,17 +75,44 @@ public class Springerproblemet {
 
             /**
              * Sjekker om det er et lovlig trekk som gjøres.
+             * Det vil si om den nye x og y er innenfor (n x n) brettet,
+             * og at den nye posisjonen er ledig og ikke vært på fra før.
              */
-            // if (nyX >= 0 && nyX <= n && ny)
+            if ( checkAvailability(nyX, nyY, sjakkBrettet) ) {
+
+                /**
+                 * Hvis finnVei med den nye x og y en er sann, altså at springeren kan gå den veien
+                 * vil vi sette posisjonen som brukt, og returnere true, som vil da gå ut av det
+                 * gjeldende funksjonskallet ( rekursjonen ).
+                 */
+                if (finnVei(nyX, nyY)) {
+                    sjakkBrettet[i][j] = BRUKT;
+                    return true;
+                }
+
+            }
         }
+
+        /**
+         * Hvis programmet har kommet hit, så fungerer ikke denne løsningen, og programmet må derfor gå tilbake (backtracking)
+         * dette gjøres i form av å sette den gjeldende posisjonen til ledig igjen.
+         */
+
+        sjakkBrettet[i][j] = LEDIG;
 
 
         return false;
     }
 
+    @Override
+    public String toString() {
+        String out = "";
+        return super.toString();
+    }
+
     public static void main(String[] args) {
         Springerproblemet springerproblemet = new Springerproblemet(5);
-        finnVei(0,0);
+        System.out.println(finnVei(0,0));
 
     }
 }
