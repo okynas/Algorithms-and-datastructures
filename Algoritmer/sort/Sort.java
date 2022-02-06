0773953394a46d74604212f0c8ac7c2941eca6f1
OkYNaS <stianmartinsen7@gmail.com>
2022-02-06 21:21:12 +0100

    FEAT: insertionsort

--- Algoritmer/sort/Sort.java
+++ Algoritmer/sort/Sort.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class Sort {
-    public static void selectionSort(int[] array) {
+    public static void SelectionSort(int[] array) {
         int n = array.length;
         int min, temp;
         for (int i = 0; i < n-1; i++) {
@@ -14,6 +14,21 @@ public class Sort {
             temp = array[min];
             array[min] = array[i];
             array[i] = temp;
+        }
+    }
+
+    public static void InsertionSort(int[] A) {
+        int n = A.length;
+        int key;
+
+        for (int i = 1; i<n; i++) {
+            key = A[i];
+            int j = i;
+            while (j >0 && A[j-1] > key) {
+                A[j] = A[j-1];
+                j--;
+            }
+            A[j] = key;
         }
     }
 }
---
+++ Algoritmer/sort/readme.md
@@ -0,0 +1,19 @@
+# Algoritme - Sort
+
+## Hva er en sorteringsalgoritme?
+
+En sorteringsalgoritme er en algoritme som plasserer elementer i en samling i en spesifikk orden
+Vanligst er å sortere stignede, ord er sortert etter alfabetet
+
+Effektiv sortering er viktig for å finne elementer i en sortert liste enklere
+
+ikke alle algoritmer kan blir brukt i alle sammenhenger.
+
+## Metoder:
+- binarySearch
+- sequentialSearch
+- interpolationSearch
+
+## Bilder:
+
+![img.png](img.png)
