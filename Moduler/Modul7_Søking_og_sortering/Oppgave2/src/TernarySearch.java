5fb669ac154c16192d8a94b0c28ff897a10e8eba
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-02-21 16:19:48 +0100

    FEAT: oppg1+2 modul7

--- Moduler/Modul7_Søking_og_sortering/Oppgave2/src/TernarySearch.java
+++ Moduler/Modul7_Søking_og_sortering/Oppgave2/src/TernarySearch.java
@@ -1,21 +1,69 @@
 public class TernarySearch {
-    public static int Iterative(int[] A, int x) {
+    public static boolean Iterative(int[] A, int x) {
         int n = A.length;
-        int i2 = n/3;
-        int i3 = 2*n/3;
 
-        if (x < i2) {
-            // elementet x ligger mellom index 0 og 1/3
-            return 1
-        } else if ( x > i2 && x < i3) {
-            // element x ligger mellom index 1/3 og 2/3
-            return 2;
-        } else {
-            // element x ligger mellom index 2/3 og n-1
-            return 3;
+        int min = 0;
+        int max = n-1;
+
+        while (n > 2) {
+            int one_third = min + n/3;
+            int two_thirds = one_third + n/3;
+
+            if (A[one_third] == x && A[two_thirds] == x) {
+                return true;
+            }
+
+            if ( x < A[one_third]) {
+                // element x ligger mellom index 1/3 og 2/3
+                max = one_third - 1;
+            } else if (x > A[two_thirds]){
+                // element x ligger mellom index 2/3 og n-1
+                min = two_thirds + 1;
+            } else {
+                min = one_third + 1;
+                max = two_thirds - 1;
+            }
+            n = max - min + 1;
         }
-        return 0;
 
+        if (n == 1) {
+            return (A[min] == x);
+        }
+        if (n == 2) {
+            return (A[min] == x || A[max] == x);
+        }
+        return false;
+
+    }
+
+    public static boolean Recursive(int[] A, int x, int min, int max) {
+        int n = A.length;
+
+        if (n > 2) {
+
+            int one_third = min + n/3;
+            int two_thirds = one_third + n/3;
+
+            if (A[one_third] == x && A[two_thirds] == x) {
+                return true;
+            }
+
+            if ( x < A[one_third]) {
+                Recursive(A, x, min, one_third - 1);
+            } else if (x > A[two_thirds]){
+                Recursive(A, x, two_thirds + 1, max);
+            } else {
+                Recursive(A, x, one_third + 1, two_thirds - 1);
+            }
+        }
+
+        if (n == 1) {
+            return (A[min] == x);
+        }
+        if (n == 2) {
+            return (A[min] == x || A[max] == x);
+        }
+        return false;
     }
 
     public static void main(String[] args) {
