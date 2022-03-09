553f0c673e2b7d27e00c68da9f5c7ec2cb2a501f
OkYNaS <stianmartinsen7@gmail.com>
2022-03-09 18:06:56 +0100

    FEAT: finishing mandatory assignment 4 - tekstanalyse

--- Obligatoriske Oppgaver/Oblig4_Tekstanalyse/src/Main.java
+++ Obligatoriske Oppgaver/Oblig4_Tekstanalyse/src/Main.java
@@ -1,36 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
+import java.io.*;
+import java.nio.Buffer;
 
 public class Main {
     /**
-     * Function will read input from user, and add new line for each "space".
-     * Got this function from task-description.
+     * Function will read input from user
      */
-    public static void readFromUserInput() {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
-        StreamTokenizer input = new StreamTokenizer(r);
-
-        try {
-            int x = input.nextToken();
-            while (x != StreamTokenizer.TT_EOF) {
-                if (input.ttype == StreamTokenizer.TT_WORD)
-                    System.out.println(input.sval);
-                x = input.nextToken ();
-            }
-        }
-        catch (IOException ignored) {};
+    public static BufferedReader readFromUserInput() {
+        return new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public static void main(String[] args) {
-        //readFromUserInput();
+    /**
+     * A method for reading data from file.
+     * @return a buffer reader, to be used in creating binary search tree nodes.
+     */
+    public static BufferedReader readFromFile()  {
+        try {
+            String basePath = new File("").getAbsolutePath();
+            FileReader in = new FileReader(basePath + "/src/input.txt");
+
+            return new BufferedReader(in);
+
+        } catch (IOException e) {
+            System.out.println("errors: " + e);
+            System.exit(1);
+        }
+
+        return null;
+    }
+
+    public static void addingToBinarySearchTree(BinarySearch BST) {
+        try {
+
+            BufferedReader br = readFromFile();
+
+            StreamTokenizer input = new StreamTokenizer(br);
+
+            int x = input.nextToken();
+            while (x != StreamTokenizer.TT_EOF) {
+                if (input.ttype == StreamTokenizer.TT_WORD) {
+
+
+                    String addingToBinarySearchTree = input.sval.toUpperCase()
+                            .replaceAll("\\.", "")
+                            .replaceAll("-", " ");
+
+                    String[] binarySearchMultiple = addingToBinarySearchTree.split(" ");
+
+                    if (binarySearchMultiple.length >= 2) {
+                        BST.insertAll(binarySearchMultiple);
+                    } else {
+                        BST.insert(addingToBinarySearchTree);
+                    }
+
+                }
+                x = input.nextToken();
+            }
+
+
+        } catch (IOException ignored) {
+            System.out.println("errors: " + ignored);
+        }
+    }
+
+    public static void main(String[] args)  {
         BinarySearch sokeTre = new BinarySearch();
-        sokeTre.insert("Stian");
-        sokeTre.insert("Martinsen");
-        sokeTre.insert("Stian");
+
+        // readFromUserInput();
+        // sokeTre.insert("Stian");
+        // sokeTre.insert("Martinsen");
+        // sokeTre.insert("Stian");
+
+        // sokeTre.inorder(sokeTre.root);
+
+        addingToBinarySearchTree(sokeTre);
 
         sokeTre.inorder(sokeTre.root);
+
+        System.out.println("\n Antall ord som er i søketreet: " + sokeTre.root.size);
 
     }
 
