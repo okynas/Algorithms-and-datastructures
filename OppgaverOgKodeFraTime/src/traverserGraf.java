cc4f230e5a6ca9b82d71cd1f2db288d91b3e5d3a
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-03-29 11:37:28 +0200

    FEAT: adding main method

--- OppgaverOgKodeFraTime/src/traverserGraf.java
+++ OppgaverOgKodeFraTime/src/traverserGraf.java
@@ -62,4 +62,31 @@ public class traverseGraf extends enkelGraf {
       }
     }
   }
+
+  public static void main(String args[]) {
+    String filnavn = "";
+    try {
+      if (args.length != 1) {
+        throw new IOException("Mangler filnavn");
+      }
+      filnavn = args[0];
+    }
+    catch(Exception e) {
+      System.err.println(e);
+      System.exit(1);
+    }
+
+    // oppretter en ny graf
+    traverseGraf G = new traverseGraf(filnavn);
+
+    System.out.println("DFS:")
+    for (int i = 0; i < G.antallNoder(); i++) {
+      G.DFS(i);
+    }
+
+    System.out.println("\n BFS:");
+    for (int i = 0; i < G.antallNoder(); i++) {
+      G.BFS(i);
+    }
+  }
 }
